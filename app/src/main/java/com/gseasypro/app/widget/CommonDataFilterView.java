7cbb32e8e46c653902713cdf3f0ac647c5e9171e
fanloveoupao <bruse_ios@163.com>
2017-02-13 23:32:10 +0800

    广师校园的界面布局

---
+++ app/src/main/java/com/gseasypro/app/widget/CommonDataFilterView.java
@@ -0,0 +1,29 @@
+package com.gseasypro.app.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by bruse on 2017/2/13.
+ */
+
+public class CommonDataFilterView extends RelativeLayout implements View.OnClickListener {
+    public CommonDataFilterView(Context context) {
+        super(context);
+    }
+
+    public CommonDataFilterView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CommonDataFilterView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
